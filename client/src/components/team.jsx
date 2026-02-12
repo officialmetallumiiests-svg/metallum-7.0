ef8ac5e32331d53ab6abaed400e3ea3d83f278a8
vinay <vinaybharat476@gmail.com>
2026-02-13 00:01:20 +0530

    ill

--- client/src/components/team.jsx
+++ client/src/components/team.jsx
@@ -178,7 +178,7 @@ export default function Team() {
       {
         name: "Pratik Raj",
         role: " Sponsorship",
-        image:"/images/pratik.jpeg",
+        image:"/images/pratik.png",
       },{
         name: "Tarishi Sharma",
         role: " Sponsorship",
