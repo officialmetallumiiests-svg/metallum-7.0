6bc43baf9fb8904eea5d7ad557eb9cc7fa8a6b9b
vinay <vinaybharat476@gmail.com>
2026-02-17 05:50:33 +0530

    ghdfghj3456cc

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,35 +16,9 @@ module.exports = {
     },
   },
 
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        light: {
-          "color-scheme": "light",
-          "primary": "#AD74C3",
-          "secondary": "#f000b8",
-          "accent": "#37cdbe",
-          "neutral": "#3d4451",
-          "base-100": "#ffffff",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
-        },
-        dark: {
-          "color-scheme": "dark",
-          "primary": "#AD74C3",
-          "secondary": "#d926a9",
-          "accent": "#1fb2a6",
-          "neutral": "#2a323c",
-          "base-100": "#1d232a",
-          "info": "#3abff8",
-          "success": "#36d399",
-          "warning": "#fbbd23",
-          "error": "#f87272",
-        },
-      },
-    ], // make sure "dark" exists for your theme toggle
+    themes: ["light", "dark"], // make sure "dark" exists for your theme toggle
   },
 };
 
